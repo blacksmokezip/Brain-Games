0fbf3d292ef45191ca7664720cfd4139f36fd1b5
blacksmoke <hanzomainer1488@gmail.com>
2023-06-14 20:32:34 +1000

    Fix brain-prime bugs

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,23 +1,22 @@
 from random import randint
+from math import sqrt
 
 
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
+  
+def get_round():
+    question = randint(0, 100)
+    answer = 'yes' if is_prime(question) else 'no'
+    return question, answer
+
 
 def is_prime(num):
-    counter = 0
-    for i in range(2, num // 3):
-        if (num % i) == 0:
-            counter += 1
-    if counter <= 0:
-        return True
-    return False
-
-
-def get_round():
-    question = randint(2, 100)
-    if is_prime(question):
-        answer = 'yes'
-    else:
-        answer = 'no'
-    return question, answer
+    if num <= 1:
+        return False
+    num_sqrt = int(sqrt(num))
+    div = range(2, (num_sqrt + 1))
+    for elem in div:
+        if num % elem == 0:
+            return False
+    return True
